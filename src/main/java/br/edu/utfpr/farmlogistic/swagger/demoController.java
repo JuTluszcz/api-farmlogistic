020aabb123204d21672da35f11769a14ba66dd71
victor-lopes29 <victoraugustus2@hotmail.com>
2024-11-21 23:11:46 -0300

    Finalização do Swagger

---
+++ src/main/java/br/edu/utfpr/farmlogistic/swagger/demoController.java
@@ -0,0 +1,16 @@
+package br.edu.utfpr.farmlogistic.swagger;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/api/")
+
+
+public class demoController {
+    @GetMapping("/demo")
+    public String demoResponse(){
+        return "swagger Implementation";
+    }
+}
